1dcef407c597d3581364e35c9c93f14e94f8104a
Nico Büscher <nico@buescher.dev>
2024-12-04 20:57:58 +0100

    03-2024 part 1

--- advent-of-code-2024/src/bin/03.rs
+++ advent-of-code-2024/src/bin/03.rs
@@ -2,13 +2,14 @@ use advent_of_code_2024::*;
 use code_timing_macros::*;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use regex::Regex;
 
-const DAY: &str = "NN"; // TODO: Fill the day
-const INPUT_FILE: &str = "input/NN.txt";
+const DAY: &str = "03";
+const INPUT_FILE: &str = "input/03.txt";
 
 const TEST: &str = "\
-<TEST-INPUT>
-"; // TODO: Add the test input
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+";
 
 fn main() -> Result<()> {
 	start_day(DAY);
@@ -16,14 +17,27 @@ fn main() -> Result<()> {
 	//region Part 1
 	println!("=== Part 1 ===");
 
+	fn read_input<R: BufRead>(reader: R) -> Vec<String> {
+		reader.lines().flatten().collect()
+	}
+	
 	fn part1<R: BufRead>(reader: R) -> Result<usize> {
-		// TODO: Solve Part 1 of the puzzle
-		let answer = reader.lines().flatten().count();
+		let input = read_input(reader);
+		
+		let answer = input.into_iter()
+			.map(|line| {
+				Regex::new(r"mul\((\d+),(\d+)\)").unwrap()
+					.captures_iter(line.as_str())
+					.map(|capture| {
+						capture[1].parse::<usize>().unwrap() * capture[2].parse::<usize>().unwrap()
+					})
+					.sum::<usize>()
+			}).sum();
+		
 		Ok(answer)
 	}
 
-	// TODO: Set the expected answer for the test input
-	assert_eq!(0, part1(BufReader::new(TEST.as_bytes()))?);
+	assert_eq!(161, part1(BufReader::new(TEST.as_bytes()))?);
 
 	let input_file = BufReader::new(File::open(INPUT_FILE)?);
 	let result = time_snippet!(part1(input_file)?);
