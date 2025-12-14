7aca0c150cafaaf9127f7560d483c943bd27d7d8
Nico Büscher <nico@buescher.dev>
2025-12-14 18:31:35 -0500

    Solve 2025, Day 7, Part 1

--- advent-of-code-2025/src/commonMain/kotlin/Day07.kt
+++ advent-of-code-2025/src/commonMain/kotlin/Day07.kt
@@ -4,7 +4,29 @@ import dev.buescher.adventofcode.core.*
 
 object Day07 : Day("2025", "07") {
 	override fun solve(input: String): Solutions {
-		val part1 = null
+		val part1 = Grid(input.lines())
+			.rows()
+			.map { it.toList() }
+			.windowed(2)
+			.fold(0) { totalSplitCount, (previousRow, row) ->
+				totalSplitCount + row.indices.fold(0) { rowSplitCount, i ->
+					when (previousRow[i].value) {
+						'S', '|' -> when (row[i].value) {
+							'^' -> {
+								row[i - 1].value = '|'
+								row[i + 1].value = '|'
+								return@fold rowSplitCount + 1
+							}
+
+							else -> {
+								row[i].value = '|'
+							}
+						}
+					}
+					rowSplitCount
+				}
+			}
+
 		val part2 = null
 
 		return Solutions(part1, part2)
--- advent-of-code-2025/src/commonMain/kotlin/package.kt
+++ advent-of-code-2025/src/commonMain/kotlin/package.kt
@@ -15,18 +15,5 @@ fun getDay(day: Int): Day = when (day) {
 	10 -> Day10
 	11 -> Day11
 	12 -> Day12
-	13 -> Day13
-	14 -> Day14
-	15 -> Day15
-	16 -> Day16
-	17 -> Day17
-	18 -> Day18
-	19 -> Day19
-	20 -> Day20
-	21 -> Day21
-	22 -> Day22
-	23 -> Day23
-	24 -> Day24
-	25 -> Day25
 	else -> throw NotImplementedError()
 }
